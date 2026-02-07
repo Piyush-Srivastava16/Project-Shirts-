bab05835d0bc095a76efa2318f44554fc87e475d
PIYUSH <PIYUSHSRIVASTAVA7275@GMAIL.COM>
2026-02-08 01:04:50 +0530

    Add files via upload

---
+++ Shirt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Shirt {
+    String brand , color;
+    double price;
+    int size;
+
+    Shirt(){
+
+    }
+     Shirt(String brand , String color , double price, int  size ){
+        this.brand=brand;
+        this.color=color;
+        this.price=price;
+        this.size=size;
+    }
+    public String toString (){
+        return "Shirts [brand ="+brand+"\tcolor is "+color +"\tprice"+price+"\tSize is "+size+"]";
+    }
+}
